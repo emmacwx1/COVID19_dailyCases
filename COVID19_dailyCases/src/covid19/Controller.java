9ba6cb0c5e52f995226bfb625f3a954980d46f20
Emma Wenxuan Chen <emmacwx@seas.upenn.edu>
2020-04-24 09:24:17 -0700

    Project Creation - Emma

---
+++ COVID19_dailyCases/src/covid19/Controller.java
@@ -0,0 +1,15 @@
+package covid19;
+
+/**
+ * Provide user daily update of confirmed cases and deaths of chosen city, country, or date
+ * @author Emma Chen &amp; Shruthi Kannan
+ *
+ */
+public class Controller {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ COVID19_dailyCases/src/covid19/Reader.java
@@ -0,0 +1,11 @@
+package covid19;
+
+/**
+ * Read daily update from github repository:
+ * Clean up, reorganize, aggregate data for controller
+ * @author Emma Chen &amp; Shruthi Kannan
+ *
+ */
+public class Reader {
+
+}
