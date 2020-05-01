d4d52671b329cfbcc16296dec304c7e854f55aa0
shruthik1709 <shruthik@seas.upenn.edu>
2020-05-01 19:20:25 -0400

    With addition of test file

---
+++ COVID19_dailyCases/src/covid19/MakeMapsTest.java
@@ -0,0 +1,68 @@
+package covid19;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MakeMapsTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testCsForDate() {
+		//Emma
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCountyStateArray() {
+		//Emma
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetDailyCase() {
+		//Emma
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetDailyDeath() {
+		//Emma
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetMonthlyCase() {
+		//Shruthi
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetMonthlyDeath() {
+		//Shruthi
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetSumCase() {
+		//Shruthi
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetSumDeath() {
+		//Shruthi
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCsForMonth() {
+		//Shruthi 
+		fail("Not yet implemented");
+	}
+
+}
